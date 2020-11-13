11ec365eb9c2763e0ba8f60b4c07607ab812e96c
jess-ward <jrnewport07@gmail.com>
2020-11-12 21:03:53 -0600

    studio 11 plus bonus

---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime JoinDate { get; }
+
+        public User(string username, string email, string password)
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+            //JoinDate = DateTime.Now;
+        }
+
+        public User() {
+
+            JoinDate = DateTime.Now;
+
+        }
+    }
+}
